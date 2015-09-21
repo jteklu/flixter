80602d66e99406c500a9b926e7f85909719c9750
Jonathan <jteklu1@umbc.edu>
2015-09-21 23:00:26 +0000

    Adjusted db, modle, and controller/view

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Flixter::Application.routes.draw do
   devise_for :users
   root 'static_pages#index'
+  namespace :instructor do
+    resources :courses, :only => [:new, :create, :show]
+  end
 
   # resources :static_pages do
     
