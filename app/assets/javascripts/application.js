52f094099d9016d797e63c68d675ba351327d276
Jonathan <jteklu1@umbc.edu>
2015-09-23 18:30:58 +0000

    Enabled VideoJS and adjusted views folder.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,8 @@
 // about supported directives.
 //
 //= require jquery
-//= require bootstrap-sprockets
 //= require jquery_ujs
-//= require turbolinks
 //= require_tree .
+// Load all Bootstrap javascripts
+//= require bootstrap
+//= require bootstrap-sprockets
